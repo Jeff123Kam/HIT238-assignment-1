47afb503d650b55195780769315658fb48087cd3
Kam <s307746@students.cdu.edu.au>
2019-10-10 23:48:09 +0930

    add sorting items function in attraction section.
    this sorting function allows user to see the ranks of the attraction from most popular to least popular
    
    Signed-off-by: Kam <s307746@students.cdu.edu.au>

--- myscript.js
+++ myscript.js
@@ -253,3 +253,23 @@ arrowRight.addEventListener("click", function() {
 startSlide();
 
 
+function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("sortall");
+    switching = true;
+    while (switching) {
+        switching = false;
+        b = list.getElementsByTagName("LI");
+        for (i = 0; i < (b.length - 1); i++) {
+            shouldSwitch = false;
+            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
+    }
+}
